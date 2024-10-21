e7c652db2ee27a940fa75e36cef1f33867960017
AsilbekKarimov <asilbekkarimov3234@gmail.com>
2024-10-21 13:50:49 +0500

    asd

--- src/middlware/uzumAuthMiddleware.js
+++ src/middlware/uzumAuthMiddleware.js
@@ -1,25 +1,39 @@
-const jwt = require('jsonwebtoken');
-const secretKey = process.env.JWT_SECRET; 
+const bcrypt = require('bcryptjs');
+const User = require("../models/userModel");
 
 const uzumAuthMiddleware = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+  if (!authHeader || !authHeader.startsWith("Basic ")) {
     return res
       .status(401)
       .json({ message: "Authorization token missing or malformed" });
   }
 
-  const token = authHeader.split(" ")[1]; 
+  const base64Credentials = authHeader.split(" ")[1];
+
+  const credentials = Buffer.from(base64Credentials, "base64").toString("ascii");
+
+  const [login, password] = credentials.split(":");
 
   try {
-    const decoded = jwt.verify(token, secretKey);
+    const user = await User.findOne({ login: login });
 
-    req.user = decoded.user;
-    next();  
+    if (!user) {
+      return res.status(401).json({ message: "Invalid login or password" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid login or password" });
+    }
+
+    req.user = { login };
+
+    next();
   } catch (error) {
     console.log(error);
-    return res.status(403).json({ message: "Invalid or expired token" });
+    res.status(500).json({ message: "Server response error. Try again later" });
   }
 };
 
