898fa1a50a8ade6c08ff1834246dadac5366efe7
Azamat1855 <azamatshomaksudov7@gmail.com>
2024-10-21 15:50:02 +0500

    ASD

--- src/controllers/generateClickUrl.js
+++ src/controllers/generateClickUrl.js
@@ -11,7 +11,7 @@ exports.generateClickPaymentUrl = (req, res) => {
     const service_id = "37390";
     const merchant_id = "12110";
     const merchant_user_id = "46320";
-    const return_url = "https://forum.norbekovgroup.uz/course-info";
+    const return_url = "https://markaz.norbekovgroup.uz/course-info";
 
     const sign_time = new Date().toISOString();
     const sign_string = crypto
@@ -23,7 +23,7 @@ exports.generateClickPaymentUrl = (req, res) => {
 
     const paymentUrl = `https://my.click.uz/services/pay/?service_id=${service_id}&merchant_id=${merchant_id}&merchant_user_id=${merchant_user_id}&transaction_param=${merchant_trans_id}&amount=${amount}&additional_param3=${course_id}&return_url=${encodeURIComponent(
         return_url
-    )}&sign_string=${sign_string}`;
+    )}`;
 
     return res.json({ paymentUrl });
 };
