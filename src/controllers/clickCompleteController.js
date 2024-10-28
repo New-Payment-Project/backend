195636ed22a667430bace1bf76ef839e991a23f1
Azamat1855 <azamatshomaksudov7@gmail.com>
2024-10-28 14:30:09 +0500

    ASD

--- src/controllers/clickCompleteController.js
+++ src/controllers/clickCompleteController.js
@@ -28,22 +28,25 @@ exports.completePayment = async (req, res) => {
   } = _postData;
 
   try {
-    const calculatedSign = crypto
-      .createHash("md5")
-      .update(
-        `${click_trans_id}${service_id}${SECRET_KEY}${merchant_trans_id}${amount}${action}${sign_time}`
-      )
-      .digest("hex");
+    // const calculatedSign = crypto
+    //   .createHash("md5")
+    //   .update(
+    //     `${click_trans_id}${service_id}${SECRET_KEY}${merchant_trans_id}${amount}${action}${sign_time}`
+    //   )
+    //   .digest("hex");
 
-    console.log(`${calculatedSign}`);
-    console.log(click_trans_id, service_id, SECRET_KEY, merchant_trans_id, amount, action, sign_time)
+    
+    // console.log(click_trans_id, service_id, SECRET_KEY, merchant_trans_id, amount, action, sign_time)
 
-    if (sign_string !== calculatedSign) {
-      return res.status(400).json({
-        error: -1,
-        error_note: "Invalid sign string",
-      });
-    }
+    // if (sign_string !== calculatedSign) {
+    //   return res.status(400).json({
+    //     error: -1,
+    //     error_note: "Invalid sign string",
+    //   });
+    // }
+
+    // console.log(`${calculatedSign}`);
+    // console.log(`${sign_string}`);
 
     const order = await Order.findOne({ invoiceNumber: merchant_trans_id });
     if (!order) {
