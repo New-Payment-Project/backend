66d3b43cc2960a71a6938f5dade3500747ae3286
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2024-10-29 18:33:38 +0500

    asdshaiufnisa

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,21 +1,32 @@
-
 const Order = require("../models/orderModel");
+const amocrmService = require("../services/amocrmServices");
 
 const getOrders = async (req, res) => {
   try {
-    const orders = await Order.find().populate("course_id")
+    const orders = await Order.find().populate("course_id");
+    for (const order of orders) {
+      if (order.status === 'ОПЛАЧЕНО') {
+        await syncOrderWithAmoCRM(order);
+      }
+    }
     res.status(200).json({ data: orders });
   } catch (error) {
     console.error("Error getting orders:", error);
     res.status(500).json({ message: "Error getting orders", error: error.message });
   }
 };
+
 const getOrderById = async (req, res) => {
   try {
     const orderId = req.params.id;
-    const order = await Order.findById(orderId).populate("course_id")
+    const order = await Order.findById(orderId).populate("course_id");
+
     if (!order) {
       return res.status(404).json({ message: "Order not found" });
+    }
+
+    if (order.status === 'ОПЛАЧЕНО') {
+      await syncOrderWithAmoCRM(order);
     }
 
     res.status(200).json({ data: order });
@@ -24,6 +35,7 @@ const getOrderById = async (req, res) => {
     res.status(500).json({ message: "Error getting order", error: error.message });
   }
 };
+
 const createOrder = async (req, res) => {
   try {
     const {
@@ -44,10 +56,12 @@ const createOrder = async (req, res) => {
       prefix
     } = req.body;
 
+    const createTimeUnix = new Date(create_time).getTime() / 1000;
+
     const newOrder = new Order({
       transactionId,
       invoiceNumber,
-      create_time,
+      create_time: createTimeUnix,
       perform_time: null,
       cancel_time: null,
       state,
@@ -66,6 +80,9 @@ const createOrder = async (req, res) => {
     });
 
     await newOrder.save();
+
+    await syncOrderWithAmoCRM(newOrder);
+
     res.status(201).json({ message: "Order created", data: newOrder });
   } catch (error) {
     console.error("Error creating order:", error);
@@ -73,16 +90,69 @@ const createOrder = async (req, res) => {
   }
 };
 
-const updateOrderStatus = async (req, res) => {
-};
+const syncOrderWithAmoCRM = async (order) => {
+  try {
+    const accessToken = await amocrmService.ensureValidAccessToken();
+    const dealName = `${order.invoiceNumber} - ${order.courseTitle}`;
+    const phone = order.clientPhone;
+    const invoiceNumber = order.invoiceNumber;
 
-const deleteOrder = async (req, res) => {
+    // Map order statuses to AmoCRM pipeline statuses
+    const statusMapping = {
+      "ВЫСТАВЛЕНО": 71258234,  // Первичный контакт
+      "ОПЛАЧЕНО": 71258222,    // Оплачено
+      "ОТМЕНЕНО": 71258226,    // Отменено
+      "НЕ ОПЛАЧЕНО": 71258230  // Не оплачено
+    };
+
+    // Set the status_id based on the order's status
+    const statusId = statusMapping[order.status] || 71258234; // Default to "ВЫСТАВЛЕНО"
+
+    // Find existing deals by client phone number
+    const existingDeals = await amocrmService.findDealByPhone(phone, accessToken);
+
+    // Check if there's an existing deal with the same invoice number
+    const matchingDeal = existingDeals.find(deal =>
+      deal.custom_fields_values?.some(f => 
+        (f.field_id === 1628273 && f.values[0].value === invoiceNumber)
+      )
+    );
+
+    // Define the data structure for creating/updating the deal
+    const dealData = {
+      "pipeline_id": 8812830,
+      "name": dealName,
+      "price": order.amount,
+      "status_id": statusId,
+      "custom_fields_values": [
+        { "field_id": 1623197, "values": [{ "value": order.clientName }] },
+        { "field_id": 1623203, "values": [{ "value": order.clientPhone }] },
+        { "field_id": 1628267, "values": [{ "value": order.tgUsername || 'нет' }] },
+        { "field_id": 1628269, "values": [{ "value": order.courseTitle }] },
+        { "field_id": 1628271, "values": [{ "value": order.status }] },
+        { "field_id": 1628273, "values": [{ "value": order.invoiceNumber }]}
+      ]
+    };
+
+    if (matchingDeal) {
+      // Update the existing deal if a match is found
+      console.log(`Updating existing deal with ID: ${matchingDeal.id} for invoiceNumber: ${invoiceNumber}`);
+      await amocrmService.updateDeal(matchingDeal.id, dealData, accessToken);
+      console.log(`Updated existing deal for course "${order.courseTitle}" with phone ${phone}.`);
+    } else {
+      // Create a new deal if no match is found
+      console.log(`Creating a new deal for phone ${phone} and name "${dealName}".`);
+      await amocrmService.createDeal([dealData], accessToken);
+      console.log(`Created new deal for course "${order.courseTitle}" with phone ${phone}.`);
+    }
+
+  } catch (error) {
+    console.error(`Error syncing order with phone ${order.clientPhone}:`, error.message);
+  }
 };
 
 module.exports = {
   getOrders,
   getOrderById,
   createOrder,
-  updateOrderStatus,
-  deleteOrder
 };
--- src/services/amocrmServices.js
+++ src/services/amocrmServices.js
@@ -2,29 +2,28 @@ const axios = require('axios');
 const Token = require('../models/amocrmTokenModel');
 
 const saveTokensToDatabase = async (accessToken, refreshToken, expiresIn) => {
-  const expiresAt = new Date(Date.now() + expiresIn * 1000); 
-
+  const expiresAt = new Date(Date.now() + expiresIn * 1000);
   await Token.findOneAndUpdate(
     {},
     { access_token: accessToken, refresh_token: refreshToken, expires_at: expiresAt },
-    { upsert: true, new: true } // Создать новый документ, если его нет
+    { upsert: true, new: true }
   );
 };
 
-// Функция для получения токенов из базы данных
+// Retrieve tokens from database
 const getTokensFromDatabase = async () => {
   return await Token.findOne();
 };
 
 const getAccessToken = async () => {
-  const code = "def50200ad6b0bef090a814a2cbf14208de9557bdbd482c6b8f133d6aafcfd6dbd0db64e08e4cd9018f6be5e6b126f47c0a0860bdc2c122c540778e10aad2fe91b6c140d445f776047515bb6ae3eab6cd3d25476cd661f87b7fb16a2fa9d30566a7704203817b0fc3563b30d6c7626b939dd4db5be36d2b271feac4574ca4f0fa7821e4e17daef5ab095314ced14686f2e60e9a498d4942b59ba03b3cf6fad8790cfa67dea96ed60bca6d04ea12f25b801bc734bbe83125b459a4afd3381b902cc497e8a352d823ecaa9f8563341fc0b5593e4b0acd0655f5792d2d5d7716074107e22949c26006aec2a5b0b12b0bf8c80e995726f654ece3705bafad1ae3198202364dc586e5ff5bc7825ab9206ea36da4821536dbeed177822c0d044cebcd56faf57b76781412242f3d01cb68d05510129467d8515f30553e7c9a9220f4232ee6ac2aaf30cd2fae991c1a62f4d62592918524e4c1a90ff7541d0c463bf0f3e79720031bbdbec8105189a6f020005cd1dcc23a4cffe9b2b9afae508b2803ec3cbc2e46b9869392c2d6a0621638831f7e93beb94dcc73012f7f5a8d6152ec76da4f7b1b3315fb07fb50187487d9a7afcc514faa37d525535768d6765268d163ef5de709cb029e25612f49fb213dc9c7f3345cb112b0e713100f261e5ea438dd8160035969f0807489ceaffa09d714e501de213f6f4f026d9583522227d2f92f0c2fde38c2fd44c80cf";
+  const code = "def50200aa6642d16d87f7899b65bfb3f56556f917269da03c3e6f176b3cefcb4823adf5b65c47353e9c5035743e04cd92adb6c86f563bd38b7de6d6ca4888dd16183396a93952e35d079fb6193f7c6575f68b247428980aa55f6c996f3c6d26d17fe59c4350bd730ac2c3d4dbeba313e606f0cee7cc04bb0894639cdfeaa776b00c82b174d5803089a228645accdbb26606a146f0fdf97016b20c7f2dc16e877c1ddb30e8394dc7c25e25bd9f05fc75349ddf48ad683ce0b7fd8646a42b0956e910ef5027a3f9e7411b3d1abcdae372f4f2ed33654efa172b37e55387d8a39713b1b4bf7672ba6f39b4248bc39e91f1ec75050f8c9f879485ba31c0f2df404c87c057cbaa4367ce14b386c102e8af5096418691490492c758cbbc3573b4614c7175d5f9a77556868b79806b7f61f329095ccee6c04507e16f27a2c2c15431cf5ff822d0baead45730e78c4f75ba8df51b3ee30fe258519b6c3c5968240abafc88f149024055ce31f0a8e89f83dc17f8263d41f8534ae464d61d22d7ea13abf3e969993ffb1db15083e8ccbb906b8e73dd522328eac88e6165f91b152b56222a9caeab7e8f7fad5f10d5666fa88a82730d43ab577bdf830bd8eee318dc64ef1ee88304f0c034fe78b75638d490a9c1afa0ba4f419a2d7258fd0cc36f370b79ef755d1717479cc49024a45a3431804ef947eb990614eb81d3e35d99"; // Replace with your authorization code
   try {
     const response = await axios.post(`https://${process.env.AMOCRM_SUBDOMAIN}.amocrm.ru/oauth2/access_token`, {
-      client_id: process.env.AMOCRM_CLIENT_ID,
-      client_secret: process.env.AMOCRM_CLIENT_SECRET,
-      grant_type: 'authorization_code',
-      code: code,
-      redirect_uri: process.env.AMOCRM_REDIRECT_URI
+      "client_id": process.env.AMOCRM_CLIENT_ID,
+      "client_secret": process.env.AMOCRM_CLIENT_SECRET,
+      "grant_type": 'authorization_code',
+      "code": code,
+      "redirect_uri": process.env.AMOCRM_REDIRECT_URI
     });
 
     const { access_token, refresh_token, expires_in } = response.data;
@@ -32,40 +31,52 @@ const getAccessToken = async () => {
     console.log('Token data saved:', access_token);
     return access_token;
   } catch (error) {
-    console.error('Error getting access token:', error.response ? error.response.data : error.message);
-    throw new Error('Failed to get access token');
+    console.error('Error getting access token:', error.response?.data || error.message);
+    throw new Error('Authorization required. Please provide a valid authorization code.');
   }
 };
+
 
 // Функция для обновления access token с помощью refresh token
 const refreshAccessToken = async () => {
   const tokens = await getTokensFromDatabase();
+  if (!tokens || !tokens.refresh_token) {
+    throw new Error('Refresh token missing. Please reauthorize to get new tokens.');
+  }
 
   try {
     const response = await axios.post(`https://${process.env.AMOCRM_SUBDOMAIN}.amocrm.ru/oauth2/access_token`, {
-      client_id: process.env.AMOCRM_CLIENT_ID,
-      client_secret: process.env.AMOCRM_CLIENT_SECRET,
-      grant_type: 'refresh_token',
-      refresh_token: tokens.refresh_token,
-      redirect_uri: process.env.AMOCRM_REDIRECT_URI
+      "client_id": process.env.AMOCRM_CLIENT_ID,
+      "client_secret": process.env.AMOCRM_CLIENT_SECRET,
+      "grant_type": 'refresh_token',
+      "refresh_token": tokens.refresh_token,
+      "redirect_uri": process.env.AMOCRM_REDIRECT_URI
     });
 
     const { access_token, refresh_token, expires_in } = response.data;
     await saveTokensToDatabase(access_token, refresh_token, expires_in);
-    console.log('Token updated:', access_token);
+    console.log('Token refreshed:', access_token);
     return access_token;
   } catch (error) {
-    console.error('Error refreshing access token:', error.response ? error.response.data : error.message);
-    throw new Error('Failed to refresh access token');
+    console.error('Error refreshing access token:', error.response?.data || error.message);
+    throw new Error('Failed to refresh token. Reauthorization required.');
   }
 };
 
-// Функция для проверки и обеспечения валидности access token
+
 const ensureValidAccessToken = async () => {
   const tokens = await getTokensFromDatabase();
-  if (!tokens || tokens.expires_at < new Date()) {
+
+  if (!tokens) {
+    console.log('No tokens found. Initiating new authorization.');
+    return await getAccessToken();
+  }
+
+  if (new Date() >= tokens.expires_at) {
+    console.log('Access token expired, attempting to refresh.');
     return await refreshAccessToken();
   }
+
   return tokens.access_token;
 };
 
@@ -122,6 +133,7 @@ const createDeal = async (newDealData) => {
         'Content-Type': 'application/json'
       }
     });
+    console.log(response.data)
 
     return response.data;
   } catch (error) {
