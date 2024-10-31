f75f2b2e9318b2ec3b286c8b513b2afac7256b8c
Azamat1855 <azamatshomaksudov7@gmail.com>
2024-10-31 17:44:03 +0500

    ASD

--- src/routes/transRoutes.js
+++ src/routes/transRoutes.js
@@ -7,6 +7,4 @@ router.get('/', getTransactions)
 router.get('/:id', getTransactionById)
 router.put('/update/:id', updateTransaction)
 router.delete('/delete/:id', deleteTransaction)
-
-    
 module.exports = router
