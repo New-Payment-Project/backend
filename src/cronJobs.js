74617fa026634050b26731be6ad32ad48fdad490
AsilbekKarimov <asilbekkarimov3234@gmail.com>
2024-12-16 09:42:59 +0500

    ADS

--- src/cronJobs.js
+++ src/cronJobs.js
@@ -1,25 +1,25 @@
-// const cron = require("node-cron");
-// const Order = require("./models/orderModel");
-// const moment = require("moment");
+const cron = require("node-cron");
+const Order = require("./models/orderModel");
+const moment = require("moment");
 
-// cron.schedule("* * * * *", async () => {
-//   console.log("Running cron job to cancel unpaid orders...");
+cron.schedule("* * * * *", async () => {
+  console.log("Running cron job to cancel unpaid orders...");
 
-//   try {
-//     const threeDaysAgo = moment().subtract(3, "days").toDate();
+  try {
+    const threeDaysAgo = moment().subtract(3, "days").toDate();
 
-//     const result = await Order.updateMany(
-//       {
-//         status: "ВЫПЛАЧЕНО",
-//         create_time: { $lte: threeDaysAgo },
-//       },
-//       {
-//         $set: { status: "ОТМЕНЕНО", cancel_time: Date.now() },
-//       }
-//     );
+    const result = await Order.updateMany(
+      {
+        status: "ВЫПЛАЧЕНО",
+        create_time: { $lte: threeDaysAgo },
+      },
+      {
+        $set: { status: "ОТМЕНЕНО", cancel_time: Date.now() },
+      }
+    );
 
-//     console.log(`Cron job completed: ${result.nModified} orders updated.`);
-//   } catch (error) {
-//     console.error("Error running cron job:", error.message);
-//   }
-// });
+    console.log(`Cron job completed: ${result.nModified} orders updated.`);
+  } catch (error) {
+    console.error("Error running cron job:", error.message);
+  }
+});
