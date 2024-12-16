beb15e8614e92054c5ec435cd9171f08118df419
AsilbekKarimov <asilbekkarimov3234@gmail.com>
2024-12-16 11:29:15 +0500

    AS

--- src/cronJobs.js
+++ src/cronJobs.js
@@ -1,25 +1,32 @@
-// const cron = require("node-cron");
-// const Order = require("./models/orderModel");
-// const moment = require("moment");
+const cron = require("node-cron");
+const moment = require("moment");
+const Order = require("./models/orderModel"); 
 
-// cron.schedule("* * * * *", async () => {
-//   console.log("Running cron job to cancel unpaid orders...");
+cron.schedule("0 0 * * *", async () => {
+  console.log("Running cron job to cancel unpaid orders...");
 
-//   try {
-//     const threeDaysAgo = moment().subtract(3, "days").toDate();
+  const threeDaysAgo = moment().subtract(3, "days").toDate();
 
-//     const result = await Order.updateMany(
-//       {
-//         status: "ВЫПЛАЧЕНО",
-//         create_time: { $lte: threeDaysAgo },
-//       },
-//       {
-//         $set: { status: "ОТМЕНЕНО", cancel_time: Date.now() },
-//       }
-//     );
+  try {
+    const ordersToUpdate = await Order.find({
+      create_time: { $lte: threeDaysAgo },
+      status: "ВЫСТАВЛЕНО", 
+    });
 
-//     console.log(`Cron job completed: ${result.nModified} orders updated.`);
-//   } catch (error) {
-//     console.error("Error running cron job:", error.message);
-//   }
-// });
+    if (ordersToUpdate.length > 0) {
+      const orderIds = ordersToUpdate.map((order) => order._id);
+      const updatedOrders = await Order.updateMany(
+        { _id: { $in: orderIds } },
+        { $set: { status: "ОТМЕНЕНО" } }
+      );
+
+      console.log(
+        `Updated ${updatedOrders.modifiedCount} orders to "ОТМЕНЕНО".`
+      );
+    } else {
+      console.log("No orders found to update.");
+    }
+  } catch (error) {
+    console.error("Error during cron job execution:", error.message);
+  }
+});
