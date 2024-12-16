53551fdd00358056fe7be7bcc3e86c3ea3e26bad
AsilbekKarimov <asilbekkarimov3234@gmail.com>
2024-12-16 09:24:20 +0500

    Added cron status

---
+++ src/cronJobs.js
@@ -0,0 +1,25 @@
+const cron = require("node-cron");
+const Order = require("./models/orderModel"); 
+const moment = require("moment");
+
+cron.schedule("0 0 * * *", async () => {
+  console.log("Running cron job to cancel unpaid orders...");
+
+  try {
+    const threeDaysAgo = moment().subtract(3, "days").toDate();
+
+    const result = await Order.updateMany(
+      {
+        status: "ВЫПЛАЧЕНО",
+        create_time: { $lte: threeDaysAgo },
+      },
+      {
+        $set: { status: "ОТМЕНЕНО", cancel_time: Date.now() },
+      }
+    );
+
+    console.log(`Cron job completed: ${result.nModified} orders updated.`);
+  } catch (error) {
+    console.error("Error running cron job:", error.message);
+  }
+});
--- src/routes/compareRoutes.js
+++ src/routes/compareRoutes.js
@@ -2,4 +2,5 @@ const express = require('express');
 const router = express.Router();
 const { getComparedOrdersAndInvoices } = require('../controllers/compareController');
 router.get('/compare-orders-invoices', getComparedOrdersAndInvoices);
+
 module.exports = router;
